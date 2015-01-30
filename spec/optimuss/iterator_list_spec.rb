b3f2e5d5a602b31dc666f67fce49313012ed881d
Bruno Bonamin <bruno@bonamin.org>
2015-01-30 11:27:25 -0300

    Specs refactoring

--- spec/optimuss/iterator_list_spec.rb
+++ spec/optimuss/iterator_list_spec.rb
@@ -2,9 +2,11 @@ require 'spec_helper'
 
 module Optimuss
   describe IteratorList do
+    let(:iterator) { Iterator.new(base_value: 2, skip_value: 1) }
+
     describe '#<<' do
       it 'appends an iterator to its list' do
-        subject << Iterator.new(base_value: 2, skip_value: 1)
+        subject << iterator
         expect(subject.members.size).to eq(1)
         expect(subject.members.first.base_value).to eq(2)
         expect(subject.members.first.skip_value).to eq(1)
@@ -13,7 +15,7 @@ module Optimuss
 
     describe '#contains?' do
       it 'returns true if contains an iterator' do
-        subject << Iterator.new(base_value: 2, skip_value: 1)
+        subject << iterator
         expect(subject.contains? 2).to eq(true)
       end
 
@@ -23,21 +25,30 @@ module Optimuss
     end
 
     describe '#increment_members_with_base_value!' do
+      let!(:base_value) { 10 }
+      let!(:skip_value) { 5 }
+      let(:iterator) do
+        Iterator.new(base_value: base_value, skip_value: skip_value)
+      end
+
       it 'moves the iterator to the next skip value' do
-        base_value, skip_value = 10, 5
-        subject << Iterator.new(base_value: base_value, skip_value: skip_value)
+        subject << iterator
         subject.increment_members_with_base_value!(base_value)
         expect(subject.members.first.base_value).to eq(base_value + skip_value)
         expect(subject.members.first.skip_value).to eq(skip_value)
       end
 
       it 'moves all iterators to the next skip value' do
-        base_value, skip_value, skip_value2 = 10, 5, 7
-        subject << Iterator.new(base_value: base_value, skip_value: skip_value)
-        subject << Iterator.new(base_value: base_value, skip_value: skip_value2)
+        other_skip_value = 7
+        subject << iterator
+        subject << Iterator.new(
+                    base_value: base_value, \
+                    skip_value: other_skip_value
+                  )
         subject.increment_members_with_base_value!(base_value)
         expect(subject.members.first.base_value).to eq(base_value + skip_value)
-        expect(subject.members.last.base_value).to eq(base_value + skip_value2)
+        expect(subject.members.last.base_value
+          ).to eq(base_value + other_skip_value)
       end
     end
   end
--- spec/optimuss/multiplication_table_spec.rb
+++ spec/optimuss/multiplication_table_spec.rb
@@ -3,29 +3,25 @@ require 'spec_helper'
 module Optimuss
   describe MultiplicationTable do
     context 'using a two dimensional array as presentation' do
+      let!(:collection)  { [1, 2, 3] }
+      let(:subject) { MultiplicationTable.new(collection: collection) }
+
+      before(:each) { subject.generate! }
+
       it 'returns a placeholder and the collection as the first row' do
-        collection = [1, 2, 3]
-        table = MultiplicationTable.new(collection: collection)
-        table.generate!
-        expect(table.product.first).to eq([nil] + collection)
+        expect(subject.product.first).to eq([nil] + collection)
       end
 
       it 'returns a placeholder and the collection as the first column' do
-        collection = [1, 2, 3]
-        table = MultiplicationTable.new(collection: collection)
-        table.generate!
-        first_column = table.product.map(&:first)
+        first_column = subject.product.map(&:first)
         expect(first_column).to eq([nil] + collection)
       end
 
       it 'returns the multiplication of rows and columns at the intersection' do
-        collection = [1, 2, 3]
-        table = MultiplicationTable.new(collection: collection)
-        table.generate!
-        expect(table.product[3][0]).to eq(3)
-        expect(table.product[0][2]).to eq(2)
-        expect(table.product[3][2]).to eq(6)
-        expect(table.product[2][3]).to eq(6)
+        expect(subject.product[3][0]).to eq(3)
+        expect(subject.product[0][2]).to eq(2)
+        expect(subject.product[3][2]).to eq(6)
+        expect(subject.product[2][3]).to eq(6)
       end
     end
   end
