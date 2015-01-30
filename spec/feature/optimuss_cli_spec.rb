d7a77d8b543fb33f359428aa1a1fb17a7319c11b
Bruno Bonamin <bruno@bonamin.org>
2015-01-30 11:50:54 -0300

    Rename optimus to optimuss

--- spec/feature/optimuss_cli_spec.rb
+++ spec/feature/optimuss_cli_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'Optimus CLI' do
+describe 'Optimuss CLI' do
   describe 'prime_table' do
     it 'runs and exits with a status 0' do
       expect(system 'bundle exec bin/optimuss prime_table').to eq(true)
